7e005d42497c23680d0b513c0c179b36f4541ab8
danneg <dana@boxalino.com>
2023-02-10 10:56:09 +0100

    [mysql] grant.sql update

--- .docker/mysql/grant.sql
+++ .docker/mysql/grant.sql
@@ -1,19 +1,19 @@
+FLUSH PRIVILEGES;
+GRANT ALL ON *.* TO 'app'@'%';
 CREATE USER 'app'@'localhost' IDENTIFIED BY 'shopware';
-CREATE USER 'app'@'%' IDENTIFIED BY 'shopware';
-CREATE USER 'root'@'localhost' IDENTIFIED BY 'root';
 GRANT ALL PRIVILEGES ON *.* TO 'app'@'%';
 GRANT ALL PRIVILEGES ON *.* TO 'app'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
 ALTER USER 'app'@'localhost' IDENTIFIED WITH mysql_native_password BY 'shopware';
 ALTER USER 'app'@'%' IDENTIFIED WITH mysql_native_password BY 'shopware';
 ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';
+ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'root';
 GRANT SUPER ON *.* TO 'app'@'%';
 GRANT SUPER ON *.* TO 'app'@'localhost';
 GRANT SUPER ON *.* TO 'root'@'localhost';
+GRANT SUPER ON *.* TO 'root'@'%';
 
 SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
 SET GLOBAL log_bin_trust_function_creators = 1;
-
-# GRANT ALL ON *.* TO 'app'@'%';
-# ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';
-# FLUSH PRIVILEGES;
